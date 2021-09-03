4ca96d6f497d32dfb5d6506f0ca79f85edc404a3
Lysytsia Oleksandr <oleksandr.lysytsia@rtl.nl>
2021-09-03 11:39:57 +0300

    make it better

--- src/App.js
+++ src/App.js
@@ -1,24 +1,54 @@
-import logo from './logo.svg';
-import './App.css';
+import css from './App.module.scss';
+import {useCallback, useState} from "react";
 
 function App() {
+    const [ km, setKm ] = useState(0);
+    const [ litresPer100km, setLitresPer100km ] = useState(0);
+    const [ celsius, setCelsius ] = useState(0);
+
+    const handleMilesToKilometers = useCallback((event) => {
+        const result = event.target.value * 1.609
+        return isFinite(result) ? setKm(result.toFixed(3)) : setKm(0)
+        // setKm(event.target.value * 1.609)
+    });
+
+    const handleMilesGalonToKmPerLitres = useCallback((event) => {
+        const firstPart = 100* 3.785;
+        const secondPart = event.target.value * 1.609;
+        const result =  firstPart/secondPart;
+        console.log(result)
+        return isFinite(result) ? setLitresPer100km(result.toFixed(3)) : setLitresPer100km(0);
+    }, [litresPer100km]);
+
+    const handleFarenheitsToCelsius = useCallback((event) => {
+        const firstPart = event.target.value - 32;
+        const secondPart = 5/9;
+        const result = firstPart * secondPart;
+        return isFinite(result) ? setCelsius(result.toFixed(3)) : setCelsius(0);
+    }, [celsius]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+   <div className={css.inputWrapper}>
+       <p>Мили в км</p>
+     <input
+         onChange={handleMilesToKilometers}
+         inputMode='numeric'
+     />
+       <span>{km}</span>
+       <p>Галоны в литры на 100 км</p>
+     <input
+         onChange={handleMilesGalonToKmPerLitres}
+         inputMode='numeric'
+     />
+       <span>{litresPer100km}</span>
+       <p>Фаренгейты в цельсии</p>
+     <input
+        onChange={handleFarenheitsToCelsius}
+        inputMode='numeric'
+     />
+       <span>{celsius}</span>
+
+   </div>
   );
 }
 
